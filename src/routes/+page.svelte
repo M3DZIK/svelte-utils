8eeb110d2640916c3cd7fcdffd09a5a8cd51b19b
M3DZIK <me@medzik.dev>
2023-11-18 18:21:46 +0100

    Format code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,24 +5,16 @@
         toastStore.trigger({
             message: 'Hello World!',
             variant
-        })
+        });
     }
 </script>
 
 <div>
-    <button on:click={showToast} class="btn">
-        Show default toast
-    </button>
+    <button on:click={showToast} class="btn"> Show default toast </button>
 
-    <button on:click={() => showToast('alert-success')} class="btn">
-        Show success toast
-    </button>
+    <button on:click={() => showToast('alert-success')} class="btn"> Show success toast </button>
 
-    <button on:click={() => showToast('alert-warning')} class="btn">
-        Show warning toast
-    </button>
+    <button on:click={() => showToast('alert-warning')} class="btn"> Show warning toast </button>
 
-    <button on:click={() => showToast('alert-error')} class="btn">
-        Show error toast
-    </button>
+    <button on:click={() => showToast('alert-error')} class="btn"> Show error toast </button>
 </div>
